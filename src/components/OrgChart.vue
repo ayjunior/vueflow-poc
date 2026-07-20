f1f02fda8c5bf3f3fe12fbc166eef6ce7b081933
ayrtonborgesjr <ayrton.borges@yahoo.com.br>
2026-07-20 10:15:48 -0300

    ajustes

--- src/components/OrgChart.vue
+++ src/components/OrgChart.vue
@@ -6,13 +6,57 @@ import { Background } from '@vue-flow/background'
 import { Controls } from '@vue-flow/controls'
 import { MiniMap } from '@vue-flow/minimap'
 import { Modal } from 'bootstrap'
-import { defaultChartData } from '../data/orgChartData'
 
 // Dimensões usadas para espaçar os nós na árvore
 const NODE_WIDTH = 200
 const NODE_HEIGHT = 90
 const H_GAP = 40
 const V_GAP = 90
+
+const defaultChartData = [
+  {
+    id: '1',
+    parentId: '',
+    siglaUnidade: 'PMC',
+    nomeUnidade: 'Prefeitura de Curitiba',
+    simboloQuantidadeCargos: 'NS-01',
+  },
+  {
+    id: '2',
+    parentId: '1',
+    siglaUnidade: 'SEC1',
+    nomeUnidade: 'Secretaria 1',
+    simboloQuantidadeCargos: 'NS-01',
+  },
+  {
+    id: '3',
+    parentId: '1',
+    siglaUnidade: 'SEC2',
+    nomeUnidade: 'Secretaria 2',
+    simboloQuantidadeCargos: 'NS-01',
+  },
+  {
+    id: '4',
+    parentId: '2',
+    siglaUnidade: 'DEP1',
+    nomeUnidade: 'Departamento 1',
+    simboloQuantidadeCargos: 'NS-01',
+  },
+  {
+    id: '5',
+    parentId: '2',
+    siglaUnidade: 'DEP2',
+    nomeUnidade: 'Departamento 2',
+    simboloQuantidadeCargos: 'NS-01',
+  },
+  {
+    id: '6',
+    parentId: '3',
+    siglaUnidade: 'DEP3',
+    nomeUnidade: 'Departamento 3',
+    simboloQuantidadeCargos: 'NS-01',
+  },
+]
 
 // Layout simples de árvore: folhas são posicionadas da esquerda para a
 // direita, e cada nó pai fica centralizado sobre seus filhos.
